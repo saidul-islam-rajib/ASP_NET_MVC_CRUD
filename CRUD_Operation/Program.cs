3c8215cd9736835f8fa64e2456a37a1097ba2f25
Saidul Islam Rajib <saidul.bd@asa-international.com>
2023-10-25 00:12:28 +0600

    4. `Injected` with `dbcontext class`

--- CRUD_Operation/Program.cs
+++ CRUD_Operation/Program.cs
@@ -1,7 +1,13 @@
+using CRUD_Operation.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<MVCDemoDbContext>(options =>
+    options.UseSqlServer(builder.Configuration
+    .GetConnectionString("MvcDemoConnectionString")));
 
 var app = builder.Build();
 
