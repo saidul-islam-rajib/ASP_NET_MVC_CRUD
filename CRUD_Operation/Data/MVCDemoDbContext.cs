0fa92364fa272278906a306a8fb5eb4dee016af8
Saidul Islam Rajib <saidul.bd@asa-international.com>
2023-10-25 00:04:03 +0600

    Created DB context class

---
+++ CRUD_Operation/Data/MVCDemoDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CRUD_Operation.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_Operation.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
